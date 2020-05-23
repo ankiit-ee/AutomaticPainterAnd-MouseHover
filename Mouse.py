91f4c2a9854bbd09fa88cda8d5694b8afd7d50d4
ankiit-ee <56360028+ankiit-ee@users.noreply.github.com>
2020-05-23 19:52:19 +0530

    Add files via upload
    Changes in mouse.py

--- Mouse.py
+++ Mouse.py
@@ -6,15 +6,15 @@ pyautogui.FAILSAFE = True
 # Location is provided as x,y pair
 # Depends on resolution of the Monitor
 # Can obtain the resolution
-# print(pyautogui.size())   // Size(width=1920, height=1080)
+print(pyautogui.size())   # Size(width=1920, height=1080)
 
 # Moving the mouse to a specific location
-# pyautogui.moveTo(100, 100, duration=0.25)
+pyautogui.moveTo(100, 100, duration=0.25)
 
 # Move the mouse in a loop
-# for i in range(10):
-#     pyautogui.moveTo(100,100,duration=0.25)
-#     pyautogui.moveTo(200, 100, duration=0.25)
-#     pyautogui.moveTo(200, 200, duration=0.25)
-#     pyautogui.moveTo(100, 200, duration=0.25)
+for i in range(10):
+    pyautogui.moveTo(100,100,duration=0.25)
+    pyautogui.moveTo(200, 100, duration=0.25)
+    pyautogui.moveTo(200, 200, duration=0.25)
+    pyautogui.moveTo(100, 200, duration=0.25)
 
